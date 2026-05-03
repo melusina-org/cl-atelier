1f6edd5c3557ea10333075495fd95c08d772795c
Michaël Le Barbier <michael.le-barbier@melusina.org>
2026-05-03 19:34:15 +0200

    rework: Opt for short package names in templates

--- libexec/lisp/development.lisp
+++ libexec/lisp/development.lisp
@@ -11,16 +11,14 @@
 ;;;; you should have received as part of this distribution. The terms
 ;;;; are also available at https://opensource.org/licenses/MIT
 
-(defpackage #:org.melusina.atelier/development
+(defpackage #:atelier/development
   (:use #:cl)
-  (:local-nicknames
-   (#:atelier #:org.melusina.atelier))
   (:export
    #:lint
    #+quicklisp
    #:reload))
 
-(in-package #:org.melusina.atelier/development)
+(in-package #:atelier/development)
 
 (defun system-relative-pathname (pathname)
   (flet ((system-source-directory ()
@@ -74,6 +72,6 @@
 ;;;;
 
 #+nil
-(org.melusina.atelier/development:reload)
+(atelier/development:reload)
 
 ;;;; End of file `development.lisp'
